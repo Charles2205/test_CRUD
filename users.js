6185b4efec62b81810242dd08f64f094c62b937c
Charles2205 <cewudzie0@gmail.com>
2023-10-02 15:19:59 +0000

    Update: created modal file

--- users.js
+++ users.js
@@ -1,1 +1,23 @@
-const {Sequelize} = require('sequelize')
+const {Sequelize,DataTypes} = require('sequelize')
+const dbConnect = require ('./dbConnect')
+const users = dbConnect.define('users',{
+    id:{
+        primaryKey:true,
+        autoIncrement: true,
+        type:DataTypes.INTEGER
+    },
+    first_name:{
+        type:DataTypes.STRING,
+        allowNull:false
+    },
+    last_name:{
+        type:DataTypes.STRING,
+        allowNull:false
+    },
+    age:{
+        type:DataTypes.INTEGER,
+        allowNull:false
+    }
+})
+users.sync({alter:true})
+module.exports= users
