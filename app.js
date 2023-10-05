f2ba92269358a889e2bdc2018b15645faa8843b0
Charles2205 <cewudzie0@gmail.com>
2023-10-05 13:18:08 +0000

    Update : frontend 2

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const  express = require('express')
 require('dotenv').config()
 const app = express()
-const port = 3000
+const port = process.env.port || 7402
 const ejs = require('ejs')
 const dbConnect = require('./dbConnect')
 const dbModel = require('./users')
